9fab1ea9aface2de7f1e1fa72c564eddc55cbb73
annieww <annie9016@hotmail.com>
2022-08-09 11:56:14 +0800

    updated style of homepage, added icon to tabbar

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -1,4 +1,5 @@
 // pages/landing/landing.js
+const DEFAULT_PAGE = 0;
 Page({
 
   /**
@@ -11,6 +12,33 @@ Page({
   /**
    * Lifecycle function--Called when page load
    */
+
+  startPageX: 0,
+  currentView: DEFAULT_PAGE,
+  data: {
+    toView: `card_${DEFAULT_PAGE}`,
+    list: ['dog#1', 'dog#2', 'cat#1', 'cat#2', 'bunny']
+  },
+
+  touchStart(e) {
+    this.startPageX = e.changedTouches[0].pageX;
+  },
+
+  touchEnd(e) {
+    const moveX = e.changedTouches[0].pageX - this.startPageX;
+    const maxPage = this.data.list.length - 1;
+    if (Math.abs(moveX) >= 150){
+      if (moveX > 0) {
+        this.currentView = this.currentView !== 0 ? this.currentView - 1 : 0;
+      } else {
+        this.currentView = this.currentView !== maxPage ? this.currentView + 1 : maxPage;
+      }
+    }
+    this.setData({
+      toView: `card_${this.currentView}`
+    });
+  },
+
   onLoad(options) {
 
   },
