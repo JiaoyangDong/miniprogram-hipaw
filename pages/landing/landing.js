58b36792ac4c30098ad70c8d1a5b541e96fab24e
annieww <annie9016@hotmail.com>
2022-08-10 10:13:39 +0800

    updated uikit on pages/index/index

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -63,7 +63,8 @@ Page({
         console.log({res})
         const pets = res.data
         page.setData({
-          pets: pets.slice(0,5)
+          // pets: pets.slice(0,5)
+          pets: pets
         })
         console.log(page.data.pets)
       }
